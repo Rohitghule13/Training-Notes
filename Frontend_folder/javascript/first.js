702df7ec4042a7ca6ac43468373523531b80b126
Rohit Ghule <rohit.ghule@mindbowser.com>
2022-03-22 09:29:52 +0530

    commit for assignment

--- Frontend_folder/javascript/first.js
+++ Frontend_folder/javascript/first.js
@@ -1,2 +1,18 @@
-const letter = new set(['a', 'b', 'c']);
-document.write(letter.size());
+function ea() {
+    document.write("this is fun <br>");
+}
+const arr = ['one', 'two', 'three'];
+document.write(arr.toString() + "<br>");
+document.write(arr.join(" -> ") + "<br>");
+document.write(arr.pop());
+document.write(arr.shift() + "<br>");
+document.write(arr);
+arr.reverse()
+document.write(arr);
+const num = [1, 3, 4];
+num.reverse();
+document.write("<br>" + num + "<br>");
+num.sort();
+document.write("<br>" + num + "<br>");
+const arrr = [40, 100, 2, 1, 0];
+document.write(arr.sort(function(a, b) { return b - a }))
