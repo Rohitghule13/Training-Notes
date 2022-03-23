ee4dce31c986a4b85106747410198c14727d2065
Rohit Ghule <rohitghule131@gmail.com>
2022-03-23 22:27:01 +0530

    work save

--- Frontend_folder/javascript_assignment/script.js
+++ Frontend_folder/javascript_assignment/script.js
@@ -1,3 +1,4 @@
+// ===== greeting function =====//
 function greeting(value) {
     console.log("im in function")
     const time = new Date();
@@ -13,14 +14,46 @@ function greeting(value) {
 
     }
 }
-document.getElementById('avatar').src = 'https://robohash.org/blanditiisquiaquo.png?size=300x300&set=set1';
-var text = fetch('https://run.mocky.io/v3/010e898c-a05c-4a0a-b947-2a65b5a267c5')
-    .then(function(res) {
-        return res.json();
-    }).then(function(data) {
-        var jsonfile = data;
-    }).catch(function(err) {
-        console.error("somthing went wrong with json");
-        console.log(error);
-    });
-console.log(jsonfile);
+
+function removePerson(row_no) {
+    console.log("im in remove fun")
+    const elem = document.getElementById(row_no);
+    elem.remove();
+}
+// Array for Person Info //
+var Arr_person = []
+    // ===== json fetch ===== //
+
+fetch("https://run.mocky.io/v3/010e898c-a05c-4a0a-b947-2a65b5a267c5").then(
+        res => {
+            res.json().then(
+                data => {
+                    console.log(data);
+                    if (data.length > 0) {
+                        var temp = "";
+                        var i = 0;
+                        data.forEach((itemData) => {
+                            temp += `<tr id="obj${i}" onclick="addEventListener('click',greeting('${itemData.first_name}'))"><td>${itemData.first_name}</td>
+                        <td>${itemData.last_name}</td>
+                        <td>${itemData.username}</td>
+                        <td>${itemData.employment.title}</td>
+                        <td>${itemData.address.country}</td>
+                        <td><button onclick="greeting('${itemData.first_name}')">view</button>
+                        <button onclick="removePerson(obj${i})">Remove</button></td></tr>`;
+                            i += 1;
+                            /*temp += "<tr>";
+                            temp += "<td>" + itemData.first_name + "</td>";
+                            temp += "<td>" + itemData.last_name + "</td>";
+                            temp += "<td>" + itemData.username + "</td>";
+                            temp += "<td>" + itemData.employment.title + "</td>"
+                            temp += "<td>" + itemData.address.country + "</td></tr>"*/
+
+
+                        });
+                        document.getElementById('data').innerHTML = temp;
+                    }
+                }
+            )
+        }
+    )
+    // ==== Right side of card ====//
