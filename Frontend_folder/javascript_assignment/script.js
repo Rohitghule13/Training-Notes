1a328fda5ff785420075273063af8d520a179947
Rohit Ghule <rohitghule131@gmail.com>
2022-03-23 16:16:39 +0530

    saving work

--- Frontend_folder/javascript_assignment/script.js
+++ Frontend_folder/javascript_assignment/script.js
@@ -1,17 +1,26 @@
-function greeting() {
+function greeting(value) {
     console.log("im in function")
     const time = new Date();
     const hur = time.getHours();
     if (hur > 5 && hur < 12) {
-        document.getElementById('greetings').innerHTML = "Good Morning";
+        document.getElementById('greetings').innerHTML = "Good Morning! " + value;
 
     } else if (hur > 12 && hur < 4) {
-        document.getElementById('greetings').innerHTML = "Good Afternoon";
+        document.getElementById('greetings').innerHTML = "Good Afternoon! " + value;
 
     } else {
-        document.getElementById('greetings').innerHTML = "Good Evening";
+        document.getElementById('greetings').innerHTML = "Good Evening!" + value;
 
     }
 }
-
-document.getElementById('leonardo').addEventListener("click", greeting);
+document.getElementById('avatar').src = 'https://robohash.org/blanditiisquiaquo.png?size=300x300&set=set1';
+var text = fetch('https://run.mocky.io/v3/010e898c-a05c-4a0a-b947-2a65b5a267c5')
+    .then(function(res) {
+        return res.json();
+    }).then(function(data) {
+        var jsonfile = data;
+    }).catch(function(err) {
+        console.error("somthing went wrong with json");
+        console.log(error);
+    });
+console.log(jsonfile);
